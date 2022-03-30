d125f909ed8c6c763d3196e69e75b1e90395bd4f
Victor Hugo Damo Pimentel <victor_hugo.d.pimentel@hotmail.com>
2022-03-30 01:32:45 -0300

    add regexes and styles to form fields

--- validation.js
+++ validation.js
@@ -1,1 +1,27 @@
 // validation script here
+const inputs = document.querySelectorAll("input");
+console.log(inputs);
+
+const patterns = {
+    telephone: /^\d{11}$/,
+    username: /^[a-z\d]{5,12}$/i,
+    password: /^[\w@-]{8,20}$/,
+    slug: /^[a-z\d-]{8,20}$/,
+    email: /^([a-z-\d\.]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+}
+
+//validation fuction
+function validate(field, regex){
+    if (regex.test(field.value)){
+        field.className = 'valid';
+    } else{
+        field.className = 'invalid';
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        validate(e.target, patterns[e.target.attributes.name.value])
+        //console.log(e.target.attributes.name.value);
+    });
+});
